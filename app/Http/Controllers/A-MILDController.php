2f86da6f483ea7552ee0d02e52491b6c537a1c6e
ZERO0611 <wahid.smkn10@gmail.com>
2015-11-23 08:37:07 +0700

    Next Commit

--- app/Http/Controllers/A-MILDController.php
+++ app/Http/Controllers/A-MILDController.php
@@ -25,6 +25,8 @@ class A-MILDController extends Controller {
 	public function create()
 	{
 		//
+		$data = \App\Cerita::all();
+		return view('buku/add')->withData($data);
 	}
 
 	/**
@@ -34,6 +36,12 @@ class A-MILDController extends Controller {
 	 */
 	public function store()
 	{
+		$addnew = new \App\Cerita;
+		$addnew->title = Input::get('title');
+		$addnew->contents = Input::get('contents');
+		$addnew->save();
+
+		return redirect(url('buku'));
 		//
 	}
 
